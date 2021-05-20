ae472e4e0444a3a6a656f992619991e6795060e0
Jonathan <pon20006@byui.edu>
2021-05-20 14:56:48 -0500

    Added my name

--- src/HelloFromAaron.java
+++ src/HelloFromAaron.java
@@ -1,6 +1,8 @@
 public class HelloFromAaron {
     public static void main(String[] args){
+
         System.out.println("Hello, my name is Aaron");
+        System.out.println("HeLLO Aaron! My name is Jonathan");
     }
 }
 
